0631baaecd8939088cd5444c56cb15e61f563bd2
enesigneci <enes.igneci@outlook.com>
2018-07-17 16:19:29 +0300

    daily forecast implementation

--- app/src/main/java/com/ahmetboluk/havadurumu/ui/main/MainActivity.java
+++ app/src/main/java/com/ahmetboluk/havadurumu/ui/main/MainActivity.java
@@ -4,14 +4,20 @@ import android.Manifest;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 
 import com.ahmetboluk.havadurumu.R;
 import com.ahmetboluk.havadurumu.model.Forecast;
 import com.ahmetboluk.havadurumu.ui.base.BaseActivity;
 
+import butterknife.BindView;
+
 public class MainActivity extends BaseActivity implements MainViewInterface {
     MainPresenter mainPresenter;
+    RecyclerView rvForecast;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +27,14 @@ public class MainActivity extends BaseActivity implements MainViewInterface {
         }
         mainPresenter = new MainPresenter(this);
         mainPresenter.getForecasts();
+        rvForecast=findViewById(R.id.five_days_forecast);
+        rvForecast.setLayoutManager(new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false));
     }
 
     @Override
     public void displayDailyForecasts(Forecast forecast) {
         Log.d("Response",forecast.getList().get(0).getMain().getTemp().toString());
+        ThreeHourForecastAdapter adapter=new ThreeHourForecastAdapter(forecast,getApplicationContext());
+        rvForecast.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/ahmetboluk/havadurumu/ui/main/ThreeHourForecastAdapter.java
+++ app/src/main/java/com/ahmetboluk/havadurumu/ui/main/ThreeHourForecastAdapter.java
@@ -6,12 +6,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ahmetboluk.havadurumu.R;
 import com.ahmetboluk.havadurumu.model.Forecast;
+import com.bumptech.glide.Glide;
 
 
-public class ThreeHourForecastAdapter extends RecyclerView.Adapter<ThreeHourForecastAdapter.MyViewHolder>{
+
+public class ThreeHourForecastAdapter extends RecyclerView.Adapter<ThreeHourForecastAdapter.DailyWeatherViewHolder>{
 
     Forecast forecast;
     Context context;
@@ -23,14 +27,16 @@ public class ThreeHourForecastAdapter extends RecyclerView.Adapter<ThreeHourFore
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public DailyWeatherViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.daily_weather_item,parent,false);
-        return new MyViewHolder(view);
+        return new DailyWeatherViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull DailyWeatherViewHolder holder, int position) {
+       holder.time.setText(position==0?"NOW":forecast.getList().get(position).getDtTxt().substring(10,16));
+       Glide.with(context).load("http://openweathermap.org/img/w/"+forecast.getList().get(position).getWeather().get(0).getIcon()+".png").into(holder.icon);
+       holder.temperature.setText(forecast.getList().get(position).getMain().getTemp().intValue()+"");
     }
 
     @Override
@@ -38,10 +44,15 @@ public class ThreeHourForecastAdapter extends RecyclerView.Adapter<ThreeHourFore
         return forecast.getList().size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-
-        public MyViewHolder(View itemView) {
+    public class DailyWeatherViewHolder extends RecyclerView.ViewHolder {
+        TextView time;
+        ImageView icon;
+        TextView temperature;
+        public DailyWeatherViewHolder(View itemView) {
             super(itemView);
+            time=itemView.findViewById(R.id.time);
+            icon=itemView.findViewById(R.id.weather_icon);
+            temperature=itemView.findViewById(R.id.temperature);
         }
     }
 }
